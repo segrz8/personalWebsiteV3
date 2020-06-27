c11307fcd2ee9bdb76c750daa7c7892c5a96877a
Sebastian Grzelak <segrz8@gmail.com>
2020-06-27 11:33:32 +0200

    Add bgcolor to form inputs

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,18 +1,86 @@
 <template>
   <section class="Contact">
     <h2 class="Contact__title">Kontakt</h2>
-    <form action></form>
+    <div class="Contact__form">
+      <div class="Contact__group">
+        <label for="name">Imię</label>
+        <input type="text" name="name" v-model="userNameInput" />
+      </div>
+      <div class="Contact__group">
+        <label for="email">E-mail</label>
+        <input type="email" name="email" v-model="userEmailInput" />
+      </div>
+      <div class="Contact__group">
+        <label for="message">Wiadomość</label>
+        <textarea name="message" v-model="userMessageInput"></textarea>
+      </div>
+      <button @click="handleSubmitForm">Wyślij</button>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+import firebase from "firebase/app";
+export default {
+  data() {
+    return {
+      userNameInput: "",
+      userEmailInput: "",
+      userMessageInput: ""
+    };
+  },
+  methods: {
+    handleSubmitForm() {
+      const firestore = firebase.firestore();
+      const db = firestore.collection("contactData");
+      if (this.userNameInput === "") {
+        console.log("uzupełnij");
+        return;
+      }
+      db.doc()
+        .set({
+          name: this.userNameInput,
+          email: this.userEmailInput,
+          message: this.userMessageInput
+        })
+        .then(function() {
+          console.log("sent");
+        })
+        .catch(function(error) {
+          alert(
+            `Coś poszło nie tak. Spróbuj ponownie później lub skontaktuj się pod adresem sebastiangrzelak87@wp.pl. Error: ${error}`
+          );
+        });
+    }
+  }
+};
 </script>
 
 <style lang="scss">
+@import "_variables.scss";
 .Contact {
   &__title {
     padding: 25px;
+  }
+  &__form {
+    padding: 25px;
+  }
+  label {
+    display: block;
+    padding: 15px 0 5px;
+  }
+  input {
+    width: 100%;
+    padding: 7px;
+    background-color: $color-secondary;
+    border: none;
+  }
+  textarea {
+    width: 100%;
+    height: 150px;
+    padding: 7px;
+    background-color: $color-secondary;
+    border: none;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase/app'
+
+require('firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCRMt8WApKlOVpl0fcBR2iBzB3J-q_qfhA",
+  authDomain: "sgpersonalwebsitev3.firebaseapp.com",
+  databaseURL: "https://sgpersonalwebsitev3.firebaseio.com",
+  projectId: "sgpersonalwebsitev3",
+  storageBucket: "sgpersonalwebsitev3.appspot.com",
+  messagingSenderId: "870423009858",
+  appId: "1:870423009858:web:8d68fc5686f0c5d45a2bdb"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore()
+
+window.db = db
 
 Vue.config.productionTip = false
 
