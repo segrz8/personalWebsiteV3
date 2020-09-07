58b8faff263bfe85392c55712b2d29cb8ab30e1e
Sebastian Grzelak <segrz8@gmail.com>
2020-09-07 18:48:10 +0200

    Add API_KEY to .env

--- src/main.js
+++ src/main.js
@@ -1,37 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import firebase from 'firebase/app'
+import Vue from "vue";
+import App from "./App.vue";
+import firebase from "firebase/app";
 
-require('firebase/firestore')
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCRMt8WApKlOVpl0fcBR2iBzB3J-q_qfhA",
-//   authDomain: "sgpersonalwebsitev3.firebaseapp.com",
-//   databaseURL: "https://sgpersonalwebsitev3.firebaseio.com",
-//   projectId: "sgpersonalwebsitev3",
-//   storageBucket: "sgpersonalwebsitev3.appspot.com",
-//   messagingSenderId: "870423009858",
-//   appId: "1:870423009858:web:8d68fc5686f0c5d45a2bdb"
-// };
+require("dotenv").config();
+require("firebase/firestore");
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAGRO3iBVIvzAPDiSJ02MAl_4Dq3qVa0oo",
+  apiKey: process.env.API_KEY,
   authDomain: "pw3-contactform.firebaseapp.com",
   databaseURL: "https://pw3-contactform.firebaseio.com",
   projectId: "pw3-contactform",
   storageBucket: "pw3-contactform.appspot.com",
   messagingSenderId: "308955436992",
-  appId: "1:308955436992:web:a63cb93817783fc7375a25"
+  appId: "1:308955436992:web:a63cb93817783fc7375a25",
 };
 
 firebase.initializeApp(firebaseConfig);
 
-var db = firebase.firestore()
+var db = firebase.firestore();
 
-window.db = db
+window.db = db;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
